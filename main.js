dd382bd40d09cbb922f76a424ffb311c56aeb4e2
Mathias-hackerman <mathias.souza@aluno.senai.br>
2025-08-06 11:44:52 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { GLTFLoader } from './three/examples/jsm/loaders/GLTFLoader.js';
 
 // Cena, câmera e renderizador
 const scene = new THREE.Scene();
